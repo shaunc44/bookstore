90d377da9fcea1f8efeaf1afb6e743a1b48e186b
Shaun <seyyes2000@hotmail.com>
2017-11-21 15:54:56 -0500

    fix import error

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -10,7 +10,7 @@ from cart.serializers import BookSerializer, OrderSerializer, OrderItemSerialize
 # from cart.permissions import IsOwnerOrReadOnly
 from rest_framework import permissions, renderers, viewsets
 from rest_framework.decorators import api_view, detail_route, list_route
-from rest_framework.response import Response, HttpResponse
+from rest_framework.response import Response
 from rest_framework.renderers import TemplateHTMLRenderer, StaticHTMLRenderer
 # from rest_framework.reverse import reverse
 from django.contrib.auth.models import User
